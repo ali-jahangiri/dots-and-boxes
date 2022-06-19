f46279791c31c9a676d1c876c3d295b166acac55
ali jahangiri <jahangiri.dev@gmail.com>
2022-06-19 22:35:53 +0430

    comment some section

--- dist/main.js
+++ dist/main.js
@@ -33,28 +33,12 @@ class Playground {
             row: {},
         };
         this.takenFilledBox = [];
+        this.createPlaygroundTemplate();
     }
     createPlaygroundTemplate() {
         const playgroundTemplate = `
         <div class="masterContainer">
-            <header>
-                <div class="headerContainer">
-                <div>
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M12,12A6,6,0,1,0,6,6,6.006,6.006,0,0,0,12,12ZM12,2A4,4,0,1,1,8,6,4,4,0,0,1,12,2Z"/><path d="M12,14a9.01,9.01,0,0,0-9,9,1,1,0,0,0,2,0,7,7,0,0,1,14,0,1,1,0,0,0,2,0A9.01,9.01,0,0,0,12,14Z"/></svg>
-                        <p>user 1</p>
-                    </div>
-                    <span>Score : </span>
-                </div>
-                <div>
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M12,12A6,6,0,1,0,6,6,6.006,6.006,0,0,0,12,12ZM12,2A4,4,0,1,1,8,6,4,4,0,0,1,12,2Z"/><path d="M12,14a9.01,9.01,0,0,0-9,9,1,1,0,0,0,2,0,7,7,0,0,1,14,0,1,1,0,0,0,2,0A9.01,9.01,0,0,0,12,14Z"/></svg>
-                        <p>user 2</p>
-                    </div>
-                    <span>Score : </span>
-                </div>
-                </div>
-            </header>
+            
             <div id="container">
                 <div id="filledBoxesAreas"></div>
             </div>
@@ -245,4 +229,4 @@ class Session {
     }
 }
 const playgroundInstance = new Playground();
-const session = new Session(playgroundInstance);
+// const session = new Session(playgroundInstance);
